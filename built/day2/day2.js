c3733bb884cbab1ac221c35c257cef587ef2fab1
Corey Van Woert <coreyvan@hey.com>
2021-12-02 19:35:16 -0600

    Day 2: now with classes

--- built/day2/day2.js
+++ built/day2/day2.js
@@ -1,10 +1,53 @@
 "use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 Object.defineProperty(exports, "__esModule", { value: true });
 var fs = require("fs");
+var Submarine = /** @class */ (function () {
+    function Submarine() {
+        this.depth = 0;
+        this.distance = 0;
+    }
+    Submarine.prototype.navigate = function (commands) {
+        var _this = this;
+        commands.forEach(function (command) {
+            switch (command.direction) {
+                case "forward":
+                    _this.distance += command.value;
+                    break;
+                case "down":
+                    _this.depth += command.value;
+                    break;
+                case "up":
+                    _this.depth -= command.value;
+                    if (_this.depth < 0)
+                        _this.depth = 0;
+                    break;
+            }
+        });
+    };
+    Submarine.prototype.getFinal = function () {
+        return this.depth * this.distance;
+    };
+    return Submarine;
+}());
 var processData = function (filename, callback) {
     fs.readFile(filename, function (err, bytes) {
         if (err)
-            callback(err, null);
+            callback(err);
         var data = bytes
             .toString()
             .split("\n")
@@ -15,53 +58,49 @@ var processData = function (filename, callback) {
         callback(null, data);
     });
 };
-var part1 = function (err, commands) {
+function part1(err, commands) {
     if (err) {
         console.log(err);
         return;
     }
-    var depth = 0;
-    var distance = 0;
-    commands.forEach(function (command) {
-        switch (command.direction) {
-            case "forward":
-                distance += command.value;
-                break;
-            case "down":
-                depth += command.value;
-                break;
-            case "up":
-                depth -= command.value;
-                if (depth < 0)
-                    depth = 0;
-                break;
-        }
-    });
-    console.log(depth * distance);
-};
+    var sub = new Submarine();
+    sub.navigate(commands);
+    console.log(sub.getFinal());
+}
+var AimedSubmarine = /** @class */ (function (_super) {
+    __extends(AimedSubmarine, _super);
+    function AimedSubmarine() {
+        var _this = _super.call(this) || this;
+        _this.aim = 0;
+        return _this;
+    }
+    AimedSubmarine.prototype.navigate = function (commands) {
+        var _this = this;
+        commands.forEach(function (command) {
+            switch (command.direction) {
+                case "forward":
+                    _this.distance += command.value;
+                    _this.depth += _this.aim * command.value;
+                    break;
+                case "down":
+                    _this.aim += command.value;
+                    break;
+                case "up":
+                    _this.aim -= command.value;
+                    break;
+            }
+        });
+    };
+    return AimedSubmarine;
+}(Submarine));
 var part2 = function (err, commands) {
     if (err) {
         console.log(err);
         return;
     }
-    var depth = 0;
-    var distance = 0;
-    var aim = 0;
-    commands.forEach(function (command) {
-        switch (command.direction) {
-            case "forward":
-                distance += command.value;
-                depth += aim * command.value;
-                break;
-            case "down":
-                aim += command.value;
-                break;
-            case "up":
-                aim -= command.value;
-                break;
-        }
-    });
-    console.log(depth * distance);
+    var sub = new AimedSubmarine();
+    sub.navigate(commands);
+    console.log(sub.getFinal());
 };
 processData("../../data/day2.txt", part1);
 processData("../../data/day2.txt", part2);
--- src/day2/day2.ts
+++ src/day2/day2.ts
@@ -5,12 +5,36 @@ type command = {
   value: number;
 };
 
+class Submarine {
+  depth: number = 0;
+  distance: number = 0;
+  navigate(commands: command[]): void {
+    commands.forEach((command) => {
+      switch (command.direction) {
+        case "forward":
+          this.distance += command.value;
+          break;
+        case "down":
+          this.depth += command.value;
+          break;
+        case "up":
+          this.depth -= command.value;
+          if (this.depth < 0) this.depth = 0;
+          break;
+      }
+    });
+  }
+  getFinal(): number {
+    return this.depth * this.distance;
+  }
+}
+
 let processData = (
   filename: string,
-  callback: (err: any, data: command[]) => void
+  callback: (err: any, data?: command[]) => void
 ) => {
   fs.readFile(filename, (err: any, bytes: Buffer) => {
-    if (err) callback(err, null);
+    if (err) callback(err);
     let data: command[] = bytes
       .toString()
       .split("\n")
@@ -22,30 +46,39 @@ let processData = (
   });
 };
 
-let part1 = (err: any, commands: command[]) => {
+function part1(err: any, commands: command[]) {
   if (err) {
     console.log(err);
     return;
   }
-  let depth = 0;
-  let distance = 0;
 
-  commands.forEach((command) => {
-    switch (command.direction) {
-      case "forward":
-        distance += command.value;
-        break;
-      case "down":
-        depth += command.value;
-        break;
-      case "up":
-        depth -= command.value;
-        if (depth < 0) depth = 0;
-        break;
-    }
-  });
-  console.log(depth * distance);
-};
+  let sub = new Submarine();
+  sub.navigate(commands);
+  console.log(sub.getFinal());
+}
+
+class AimedSubmarine extends Submarine {
+  aim: number = 0;
+  constructor() {
+    super();
+  }
+  navigate(commands: command[]) {
+    commands.forEach((command) => {
+      switch (command.direction) {
+        case "forward":
+          this.distance += command.value;
+          this.depth += this.aim * command.value;
+          break;
+        case "down":
+          this.aim += command.value;
+          break;
+        case "up":
+          this.aim -= command.value;
+          break;
+      }
+    });
+  }
+}
 
 let part2 = (err: any, commands: command[]) => {
   if (err) {
@@ -53,25 +86,9 @@ let part2 = (err: any, commands: command[]) => {
     return;
   }
 
-  let depth = 0;
-  let distance = 0;
-  let aim = 0;
-
-  commands.forEach((command) => {
-    switch (command.direction) {
-      case "forward":
-        distance += command.value;
-        depth += aim * command.value;
-        break;
-      case "down":
-        aim += command.value;
-        break;
-      case "up":
-        aim -= command.value;
-        break;
-    }
-  });
-  console.log(depth * distance);
+  let sub = new AimedSubmarine();
+  sub.navigate(commands);
+  console.log(sub.getFinal());
 };
 
 processData("../../data/day2.txt", part1);
